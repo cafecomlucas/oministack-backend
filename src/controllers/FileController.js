2b675f5db42df0ee6f5b557f927bb6c603b83f54
Lucas Soares <cafecomlucas@gmail.com>
2019-04-12 12:44:02 -0300

    Corrigindo com ESLint

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,20 +1,20 @@
-const Box = require("../models/Box");
-const File = require("../models/File");
+const Box = require('../models/Box');
+const File = require('../models/File');
 
 class FileController {
-  async store(req, res) {
+  static async store(req, res) {
     const box = await Box.findById(req.params.id);
     // Cria um arquivo
     const file = await File.create({
       title: req.file.originalname,
-      path: req.file.key
+      path: req.file.key,
     });
 
     box.files.push(file);
 
     await box.save();
 
-    req.io.sockets.in(box._id).emit("file", file);
+    req.io.sockets.in(box._id).emit('file', file);
 
     return res.json(file);
   }
